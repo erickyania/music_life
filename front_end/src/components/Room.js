0a2b9f8f5f8b3888b6ab9403fd2d5c82b5f17337
eric kyania <erickyania0@gmail.com>
2024-11-12 14:10:40 +0300

    made changes to some files

--- front_end/src/components/Room.js
+++ front_end/src/components/Room.js
@@ -1,18 +1,22 @@
-import React, { Component } from "react"
-export default class Room extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            votesToSkip: 2,
-            guestCanPause: false,
-            isHost: false,
-        };
-    }
-    render() {
-        return <div>
-            <p>Votes:{this.state.votes_to_skip}</p>
-            <p>Guest can pause:{this.state.guestCsnPause}</p>
-            <p>Host:{this.state.isHost</p>
-        </div>
-    }
+import React, { Component } from "react";
+export default class Room extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votesToSkip: 2,
+      guestCanPause: false,
+      isHost: false,
+    };
+    this.roomCode = this.props.match.params.roomCode;
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.roomCode}</h3>
+        <p>Votes:{this.state.votes_to_skip}</p>
+        <p>Guest can pause:{this.state.guestCsnPause}</p>
+        <p>Host:{this.state.isHost}</p>
+      </div>
+    );
+  }
 }
