ce89ea9df5fbee03058b19d1cf7a06e762d99542
eric kyania <erickyania0@gmail.com>
2024-11-14 23:38:26 +0300

    added more logic to the room page

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 """points to an endpoint"""
 from django.urls import path 
-from .views import RoomView,CreateRoomView,getRoom,JoinRoom
+from .views import RoomView,CreateRoomView,getRoom,JoinRoom,UserInRoom
 
 
 urlpatterns = [
     path('room',RoomView.as_view()),
     path('create-room', CreateRoomView.as_view()),
     path('get-room',getRoom.as_view()),
-    path('join-room', JoinRoom.as_view())
+    path('join-room', JoinRoom.as_view()),
+    path('user-in-room',UserInRoom.as_view())
 ]
--- front_end/src/components/Room.js
+++ front_end/src/components/Room.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
 
 export default class Room extends Component {
   constructor(props) {
@@ -8,16 +9,24 @@ export default class Room extends Component {
       guestCanPause: false,
       isHost: false,
     };
-    this.roomCode = this.props.roomCode;
+    this.roomCode = this.props.roomCode; // use the roomCode passed as prop
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
   }
 
   componentDidMount() {
     this.getRoomDetails();
   }
 
+  // Fetch room details from the backend
   getRoomDetails() {
     fetch("/api/get-room?code=" + this.roomCode)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/"); // Redirect if room is not found
+        }
+        return response.json();
+      })
       .then((data) => {
         this.setState({
           votesToSkip: data.votes_to_skip,
@@ -27,14 +36,51 @@ export default class Room extends Component {
       });
   }
 
+  // Handle leave room button press
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback(); // Callback to handle leaving room in parent component
+      this.props.history.push("/"); // Redirect to homepage
+    });
+  }
+
   render() {
     return (
-      <div>
-        <h3>{this.roomCode}</h3>
-        <p>Votes: {this.state.votesToSkip}</p>
-        <p>Guest can pause: {String(this.state.guestCanPause)}</p>
-        <p>Host: {String(this.state.isHost)}</p>
-      </div>
+      <Grid container spacing={1} alignItems="center" justify="center">
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes to Skip: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest Can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
     );
   }
 }
